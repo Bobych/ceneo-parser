60ad8b0a94dcd32185c669a108c72a55d6799138
boobych <nikitagoder@gmail.com>
2025-11-15 00:14:34 +0300

    ref: imports in google module

--- src/google/google.module.ts
+++ src/google/google.module.ts
@@ -1,11 +1,7 @@
+import { GoogleService } from './google.service';
 import { Module } from '@nestjs/common';
 
-import { GoogleService } from './google.service';
-import { RedisModule } from '@/redis/redis.module';
-import { LoggerModule } from '@/logger/logger.module';
-
 @Module({
-    imports: [RedisModule, LoggerModule],
     providers: [GoogleService],
     exports: [GoogleService],
 })
--- src/google/google.service.ts
+++ src/google/google.service.ts
@@ -1,25 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { ExportData } from '@/interfaces/ExportDataInterface';
 import { ConfigService } from '@nestjs/config';
 import { google, sheets_v4 } from 'googleapis';
-import * as fs from 'fs';
-import * as path from 'path';
-import ExcelJS from 'exceljs';
-
+import { Injectable } from '@nestjs/common';
+import { existsSync, mkdirSync } from 'fs';
 import { ENV } from '@/constants';
-import { ExportData } from '@/interfaces/ExportDataInterface';
-import { RedisService } from '@/redis/redis.service';
-import { LoggerService } from '@/logger/logger.service';
+import { join } from 'path';
 
 @Injectable()
 export class GoogleService {
     private sheets: sheets_v4.Sheets;
-    private readonly filePath: string;
 
-    constructor(
-        private readonly config: ConfigService,
-        private readonly redis: RedisService,
-        private readonly logger: LoggerService,
-    ) {
+    constructor(private readonly config: ConfigService) {
         const auth = new google.auth.GoogleAuth({
             keyFile: this.config.get<string>(ENV.GOOGLE_APPLICATION_CREDENTIALS),
             scopes: [
@@ -29,34 +20,21 @@ export class GoogleService {
         });
 
         this.sheets = google.sheets({ version: 'v4', auth });
-        this.filePath = path.join(__dirname, '..', '..', 'public', 'actual.xlsx');
         this.ensurePublicDirectoryExists();
         this.ensureArchiveDirectoryExists();
     }
 
     private ensurePublicDirectoryExists(): void {
-        const publicDir = path.join(__dirname, '..', '..', 'public');
-        if (!fs.existsSync(publicDir)) {
-            fs.mkdirSync(publicDir, { recursive: true });
+        const publicDir = join(__dirname, '..', '..', 'public');
+        if (!existsSync(publicDir)) {
+            mkdirSync(publicDir, { recursive: true });
         }
     }
 
     private ensureArchiveDirectoryExists(): void {
-        const archiveDir = path.join(__dirname, '..', '..', 'public', 'archive');
-        if (!fs.existsSync(archiveDir)) {
-            fs.mkdirSync(archiveDir, { recursive: true });
-        }
-    }
-
-    private async ensureActualExcelFileExists(): Promise<void> {
-        if (!fs.existsSync(this.filePath)) {
-            const workbook = new ExcelJS.Workbook();
-            await workbook.xlsx.writeFile(this.filePath);
-
-            await this.logger.set({
-                service: 'google',
-                message: 'Создан актуальный Excel файл: actual.xlsx',
-            });
+        const archiveDir = join(__dirname, '..', '..', 'public', 'archive');
+        if (!existsSync(archiveDir)) {
+            mkdirSync(archiveDir, { recursive: true });
         }
     }
 
@@ -102,30 +80,6 @@ export class GoogleService {
         }
     }
 
-    async getUidRow(uid: string): Promise<ExportData> {
-        try {
-            const range = `${this.config.get<string>(ENV.EXPORT_SHEET_ID)}!A${uid}:Z${uid}`;
-
-            const response = await this.sheets.spreadsheets.values.get({
-                spreadsheetId: this.config.get<string>(ENV.EXPORT_SPREADSHEET_ID),
-                range: range,
-            });
-
-            const { values } = response.data;
-
-            return values.map(([uid, name, url]) => ({
-                uid,
-                name,
-                url,
-            }))[0];
-        } catch (error) {
-            await this.logger.set({
-                service: 'google',
-                message: error,
-            });
-        }
-    }
-
     async getLastUidRow(): Promise<ExportData> {
         try {
             let uid = await this.getLastUid();
@@ -156,134 +110,7 @@ export class GoogleService {
                 url,
             }))[0];
         } catch (error) {
-            await this.logger.set({
-                service: 'google',
-                message: error,
-            });
-        }
-    }
-
-    async moveSheetToArchive() {
-        try {
-            await this.ensureActualExcelFileExists();
-            const now = new Date();
-            const timestamp = now
-                .toISOString()
-                .replace(/[:.]/g, '-')
-                .replace('T', '_')
-                .split('Z')[0];
-            const archiveTableName = `export-data-${timestamp}.xlsx`;
-
-            const srcPath = this.filePath;
-            const archiveDir = path.join(__dirname, '..', '..', 'public', 'archive');
-            const dstPath = path.join(archiveDir, archiveTableName);
-
-            fs.copyFileSync(srcPath, dstPath);
-
-            const files = fs.readdirSync(archiveDir);
-
-            if (files.length > 10) {
-                const sortedFiles = files
-                    .map(file => ({
-                        name: file,
-                        time: fs.statSync(path.join(archiveDir, file)).mtime.getTime(),
-                    }))
-                    .sort((a, b) => a.time - b.time);
-
-                while (sortedFiles.length > 10) {
-                    const fileToDelete = sortedFiles.shift();
-                    fs.unlinkSync(path.join(archiveDir, fileToDelete.name));
-                    await this.logger.set({
-                        service: 'google',
-                        message: `Удалён старый архивный Excel файл: ${fileToDelete.name}`,
-                    });
-                }
-            }
-
-            await this.logger.set({
-                service: 'google',
-                message: `Новый архивный Excel файл: ${archiveTableName}`,
-            });
-        } catch (error) {
-            await this.logger.set({
-                service: 'google',
-                message: error,
-            });
-        }
-    }
-
-    private async sheetExists(sheetName: string): Promise<boolean> {
-        await this.ensureActualExcelFileExists();
-        const workbook = new ExcelJS.Workbook();
-        await workbook.xlsx.readFile(this.filePath);
-        return workbook.worksheets.some((ws: { name: string }) => ws.name === sheetName);
-    }
-
-    async createSheet(sheetName: string) {
-        const workbook = new ExcelJS.Workbook();
-
-        try {
-            await this.ensureActualExcelFileExists();
-            await workbook.xlsx.readFile(this.filePath);
-        } catch (error) {
-            if (error.code !== 'ENOENT') throw error;
-        }
-
-        workbook.addWorksheet(sheetName);
-        await workbook.xlsx.writeFile(this.filePath);
-
-        await this.logger.set({
-            service: 'google',
-            message: `Создан новый лист: ${sheetName}.`,
-        });
-    }
-
-    async insertData(sheetName: string, data: any[][]): Promise<void> {
-        try {
-            sheetName = sheetName.substring(0, 31);
-            if (!(await this.sheetExists(sheetName))) {
-                await this.createSheet(sheetName);
-            }
-
-            const workbook = new ExcelJS.Workbook();
-            await workbook.xlsx.readFile(this.filePath);
-
-            let worksheet = workbook.getWorksheet(sheetName);
-            if (!worksheet) {
-                worksheet = workbook.addWorksheet(sheetName);
-            }
-
-            if (data.length === 1 && data[0].length === 0) {
-                workbook.removeWorksheet(sheetName);
-                workbook.addWorksheet(sheetName);
-                await workbook.xlsx.writeFile(this.filePath);
-                return;
-            }
-
-            const idIndex = 0;
-            const existingRows = worksheet.getRows(1, worksheet.rowCount) || [];
-
-            data.forEach(newRow => {
-                const newId = newRow[idIndex];
-                const existingRow = existingRows.find(
-                    row => row.getCell(idIndex + 1).value === newId,
-                );
-
-                if (existingRow) {
-                    newRow.forEach((value, colIndex) => {
-                        existingRow.getCell(colIndex + 1).value = value;
-                    });
-                } else {
-                    worksheet.addRow(newRow);
-                }
-            });
-
-            await workbook.xlsx.writeFile(this.filePath);
-        } catch (error) {
-            await this.logger.set({
-                service: 'google',
-                message: error,
-            });
+            console.error(error);
         }
     }
 }
